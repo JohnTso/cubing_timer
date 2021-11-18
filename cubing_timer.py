adfdff5a5d43e7e535233a9ca123a5ffe865acf6
John Tso <tsotcj@gmail.com>
2021-11-17 22:41:11 -0800

    scrolling, plus 2, dnf added

--- cubing_timer.py
+++ cubing_timer.py
@@ -3,7 +3,7 @@ import pygame, time, sys, random
 
 print("initializing...")
 
-WIDTH = 700
+WIDTH = 780
 HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0 ,0, 0)
@@ -13,7 +13,6 @@ RED = (255, 0, 0)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('professional cubing timer')
-
 def timer():
 
     print("loading...")
@@ -93,8 +92,16 @@ def timer():
         return f'{s}'
         
     def draw_info(input):
+        dnf_list = []
+        l = len(input)
+        for i in range(len(input)):
+            if not info[i+1][2] % 2:
+                dnf_list.append(i)
         sec_list = []
         for i in input:
+            if input.index(i) in dnf_list:
+                l-= 1
+                continue
             sec = 0
             for j in range(len(i)):
                 if i[j] and j == 0:
@@ -104,16 +111,19 @@ def timer():
                 elif i[j] and j == 2:
                     sec += i[j]
             sec_list.append(sec)
-                
+
+        if sec_list == []:
+            draw_info(input) 
+
         mean = round(sum(sec_list)/len(sec_list),2)
         mean_time = sec_to_time(mean)
         best = min(sec_list)
         best_time = sec_to_time(best)
         ao5 = '-'
         ao12 = '-'
-        if len(input) > 4:
+        if l > 4:
             ao5 = sec_to_time(round(sum(sec_list[-5:])/5, 2))
-        if len(input) > 11:
+        if l > 11:
             ao12 = sec_to_time(round(sum(sec_list[-12:])/12, 2))
 
         draw_text(screen, "Mean: " + mean_time, 25, 155, 400, BLACK)
@@ -162,23 +172,30 @@ def timer():
     scramble_info = scramble(cube_size)
     draw_text(screen, f"{cube_size}x{cube_size}", 20, 250, 10, BLACK)
     draw_text(screen, "scramble:" + scramble_info, 21, 255, 40, BLACK)
-    draw_text(screen, '#', 25, x+15, y+5, BLACK)
-    draw_text(screen, 'times', 25, x+90, y+5, BLACK)
     pygame.draw.rect(screen, BLACK, pygame.Rect(x,y,30,30), 2)
     pygame.draw.rect(screen, BLACK, pygame.Rect(x+30,y,125,30), 2)
-    pygame.display.update()
+    font = pygame.font.SysFont("roman", 25)
+    n = font.render("#", True, BLACK)
+    t = font.render("times", True, BLACK)
+    screen.blit(n, (x+10,y+5))
+    screen.blit(t, (x+70,y+5))
+    pygame.display.flip()
 
     screen.fill(WHITE)
     init = True
     while init:
                         
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
+
                 pygame.quit()
                 sys.exit()
 
             if event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_SPACE:
+
                     pygame.display.update()
                     draw_text(screen, 'Ready...', 40, 325, 100, RED)
                     draw_text(screen, '0.0', 70, 325, 200, RED, True)
@@ -219,7 +236,7 @@ def timer():
                                     timing = False
                                     
                         seconds_to_2 = str(round(time.time() - before, 2))
-                        seconds = str(round(time.time() - before, 1))
+                        seconds = str(round(float(seconds_to_2), 1))
 
                         if seconds == '60.0':
                             minutes += 1
@@ -232,27 +249,64 @@ def timer():
 
                         screen.fill(WHITE)
                         draw_text(screen, draw_time(seconds, minutes, hours), 80, 325, 200, BLACK, True)
-                        pygame.display.update()
+                        pygame.display.flip()
 
-                    def draw_solves(solves, info):
-                        y = 15
+                    def draw_solves(solves, info, y=15):
+
                         x = 530
-                        pygame.draw.rect(screen, BLACK, pygame.Rect(x,y,30,30), 2)
-                        draw_text(screen, '#', 25, x+15, y+5, BLACK)
-                        draw_text(screen, 'times', 25, x+90, y+5, BLACK)
-                        pygame.draw.rect(screen, BLACK, pygame.Rect(x+30,y,125,30), 2)
+                        pygame.draw.rect(screen, BLACK, pygame.Rect(520,y,40,30), 2)
+                        pygame.draw.rect(screen, BLACK, pygame.Rect(560,y,125,30), 2)
+                        n = font.render("#", True, BLACK)
+                        t = font.render("times", True, BLACK)
+                        screen.blit(n, (540,y+5))
+                        screen.blit(t, (600,y+5))
+                        #draw_text(screen, "#", 25, 540, 20, BLACK)
+                        #draw_text(screen, "times", 25, 600, 20, BLACK)
+                        global rect_pos_p2
+                        global rect_pos_dnf
+                        rect_pos_p2 = {}
+                        rect_pos_dnf = {}
+
                         for i in range(solves):
+
+                            i += 1
                             y += 30
-                            draw_text(screen, str(i+1), 25, x+15, y+5, BLACK)
-                            draw_text(screen, str(info[i+1]), 25, x+90, y+5, BLACK)
-                            pygame.draw.rect(screen, BLACK, pygame.Rect(530,y,30,30), 2)
-                            pygame.draw.rect(screen, BLACK, pygame.Rect(560,y,125,30), 2)
+                            n = font.render(str(i), True, BLACK)
+                            plus_2 = font.render("+2", True, BLACK)
+                            dnf = font.render("DNF", True, BLACK)
+                            x1 = x + 155
+                            y1 = y + 5
+                            screen.blit(plus_2, (x1+2,y1))
+                            screen.blit(dnf, (x1+32,y1))
+                            pygame.draw.rect(screen, BLACK, pygame.Rect(x-10,y,40,30), 2)
+                            pygame.draw.rect(screen, BLACK, pygame.Rect(x+30,y,125,30), 2)
+                            pygame.draw.rect(screen, RED, pygame.Rect(x1,y,30,30), 2)
+                            pygame.draw.rect(screen, RED, pygame.Rect(x1+30,y,55,30), 2)
+                            rect_pos_p2[i-1] = [x1,y,i]
+                            rect_pos_dnf[i-1] = [x1+30,y,i]
+
+                            if not info[i][1] % 2:
+                                n = font.render(str(i), True, BLACK)
+                                pygame.draw.rect(screen, GREEN, pygame.Rect(x1,y,30,30), 2)
+
+                            elif not info[i][2] % 2:
+                                t = font.render("DNF", True, BLACK)
+                                pygame.draw.rect(screen, GREEN, pygame.Rect(x1+30,y,55,30), 2)
+
+                            else:
+                                t = font.render(str(info[i][0]), True, BLACK)
+
+                            screen.blit(t, (x+70,y+5))
+                            screen.blit(n, (x,y+5))
+
+
 
                     if not timing:
+
                         solves += 1
                         screen.fill(WHITE)
                         times.append([int(hours),int(minutes),float(seconds_to_2)])
-                        info[solves] = draw_time(float(seconds_to_2), int(minutes), int(hours))
+                        info[solves] = [draw_time(float(seconds_to_2), int(minutes), int(hours)), 1, 1]
                         draw_solves(solves, info)
                         draw_text(screen, 'Press space again to start', 25, 250, 100, RED)
                         scramble_info = scramble(cube_size)
@@ -263,19 +317,97 @@ def timer():
                         draw_text(screen, f"solve #{solves}", 25, 250, 570, BLACK)
                         pygame.display.update()
 
+                    mover = 0
                     while not timing:
+
                         for event in pygame.event.get():
+
                             if event.type == pygame.QUIT:
                                 pygame.quit()
                                 sys.exit()
 
                             if event.type == pygame.KEYDOWN:
+
                                 if event.key == pygame.K_SPACE:
+
                                     screen.fill(WHITE)
                                     draw_text(screen, 'Ready...', 40, 325, 100, RED)
                                     draw_text(screen, '0.0', 70, 325, 200, RED, True)
                                     draw_text(screen, "Previous time: "+draw_time(seconds_to_2, minutes, hours), 35, 325, 300, BLACK)
                                     pygame.display.update()
                                     timing = True
+
+                            mouse = pygame.mouse.get_pos()
+                            for i in range(solves):
+
+                                x_p2 = rect_pos_p2[i][0]
+                                y_p2 = rect_pos_p2[i][1]
+                                x_dnf = rect_pos_dnf[i][0]
+                                y_dnf = rect_pos_dnf[i][1]
+
+                                if event.type == pygame.MOUSEBUTTONDOWN:
+                                    
+                                    if x_p2 < mouse[0] < x_p2+30 and y_p2 < mouse[1] < y_p2+30 and pygame.mouse.get_pressed()[0]:
+
+                                        if not info[i+1][1] % 2:
+
+                                            info[i+1][1] += 1
+                                            info[rect_pos_p2[i][2]][0] -= 2
+                                            times[i][2] -= 2
+                                            info[rect_pos_p2[i][2]][0] = round(info[rect_pos_p2[i][2]][0], 2)
+                                            times[i][2] = round(times[i][2], 2)
+                                            pygame.draw.rect(screen, WHITE, pygame.Rect(520,15,250,HEIGHT))
+                                            draw_solves(solves, info, 15+mover)
+                                            pygame.draw.rect(screen, RED, pygame.Rect(x_p2,y_p2,30,30), 2)
+                                            pygame.display.flip()
+                                            continue
+
+                                        if info[i+1][1] % 2:
+                                            info[i+1][1] += 1
+                                            info[rect_pos_p2[i][2]][0] += 2
+                                            times[i][2] += 2
+                                            info[rect_pos_p2[i][2]][0] = round(info[rect_pos_p2[i][2]][0], 2)
+                                            times[i][2] = round(times[i][2], 2)
+                                            pygame.draw.rect(screen, WHITE, pygame.Rect(520,15,250,HEIGHT))
+                                            draw_solves(solves, info, 15+mover)
+                                            pygame.draw.rect(screen, GREEN, pygame.Rect(x_p2,y_p2,30,30), 2)
+                                            pygame.display.flip()
+
+                                    if x_dnf < mouse[0] < x_dnf+55 and y_dnf < mouse[1] < y_dnf+30 and pygame.mouse.get_pressed()[0]:
+
+                                        if not info[i+1][2] % 2:
+
+                                            info[i+1][2] += 1 
+                                            pygame.draw.rect(screen, WHITE, pygame.Rect(520,15,250,HEIGHT))
+                                            draw_solves(solves, info, 15+mover)
+                                            pygame.draw.rect(screen, RED, pygame.Rect(x_dnf,y_dnf,55,30), 2)
+                                            pygame.display.flip()
+                                            continue
+
+                                        elif info[i+1][2] % 2:
+
+                                            info[i+1][2] += 1 
+                                            pygame.draw.rect(screen, WHITE, pygame.Rect(520,15,250,HEIGHT))
+                                            draw_solves(solves, info, 15+mover)
+                                            pygame.draw.rect(screen, GREEN, pygame.Rect(x_dnf,y_dnf,55,30), 2)
+                                            pygame.display.flip()
+
+                                    if event.button == 4:
+                                        mover += 3
+
+                                    elif event.button == 5:
+                                        mover -= 3
+
+                                    if 530 < mouse[0] < WIDTH and 15 < mouse[1] < HEIGHT:
+                                        pygame.draw.rect(screen, WHITE, pygame.Rect(510,0,300,HEIGHT))
+                                        draw_solves(solves, info, 15+mover)
+                                        pygame.display.flip()
+                                       
+                                
+                                        
+                                
+                                
+
+
                             
 timer()
